f7e9a8370bc637e18f24de0db264be833ff30767
Денис <layon17@outlook.com>
2020-01-17 03:48:57 +0800

    Добавление возможности логирования на всех участках кода

--- MuloApi/Startup.cs
+++ MuloApi/Startup.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace MuloApi
 {
     public class Startup
     {
+        public static ILogger<Program> LoggerApp;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,8 +24,9 @@ namespace MuloApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Program> logger)
         {
+            LoggerApp = logger;
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
