4a32840af90777417f35e8ed2120e9a3da047a42
Денис <layon17@outlook.com>
2020-01-28 02:06:45 +0800

    Первый этап создания модуля загрузки загрузки файла на сервер

---
+++ MuloApi/Controllers/ActionsOnFilesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MuloApi.Models;
+
+namespace MuloApi.Controllers
+{
+    [ApiController]
+    public class ActionsOnFilesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/user/{idUser:min(0)}/soundtracks/upload")]
+        public async Task<JsonResult> UploadSoundTrack(int idUser)
+        {
+            return new JsonResult(new
+            {
+                tracks = new ModelUserTracks
+                {
+                    Id = 5,
+                    Name = "The Score - Rush"
+                }
+            });
+        }
+    }
+}
