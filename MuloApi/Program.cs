cfd9ce1eae4186ae88f3683a117f32c9b4cbb3b8
Денис <layon17@outlook.com>
2020-02-11 02:22:00 +0800

    Рефакторинг: Удаление настроек асинхронности для метода Main

--- MuloApi/Program.cs
+++ MuloApi/Program.cs
@@ -1,14 +1,12 @@
 using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace MuloApi
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
